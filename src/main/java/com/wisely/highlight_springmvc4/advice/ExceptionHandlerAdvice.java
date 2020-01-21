d8b6f0b2cd35c92a2a3086e0a3c889f0371d2436
ycy-ycy <2923616405@qq.com>
2020-01-21 20:58:16 +0800

    Update

---
+++ src/main/java/com/wisely/highlight_springmvc4/advice/ExceptionHandlerAdvice.java
@@ -0,0 +1,37 @@
+//IntelliJ IDEA
+//highlight_springmvc4
+//ExceptionHandlerAdvice
+//2020/1/21
+// Author:御承扬
+//E-mail:2923616405@qq.com
+
+package com.wisely.highlight_springmvc4.advice;
+
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView exception(Exception exception, WebRequest request){
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("errorMessage",exception.getMessage());
+        return modelAndView;
+    }
+    @ModelAttribute
+    public void addAttributes(Model model){
+        model.addAttribute("msg","额外信息");
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.setDisallowedFields("id");
+    }
+}
---
+++ src/main/java/com/wisely/highlight_springmvc4/web/ch4_4/AdviceController.java
@@ -0,0 +1,22 @@
+//IntelliJ IDEA
+//highlight_springmvc4
+//AdviceController
+//2020/1/21
+// Author:御承扬
+//E-mail:2923616405@qq.com
+
+package com.wisely.highlight_springmvc4.web.ch4_4;
+
+
+import com.wisely.highlight_springmvc4.domain.DemoObj;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdviceController {
+    @RequestMapping("/advice")
+    public String getSomething(@ModelAttribute("msg") String msg, DemoObj obj) {
+        throw new IllegalArgumentException("非常抱歉，参数有误/" + "来自@ModelAtrribute:" + msg);
+    }
+}
